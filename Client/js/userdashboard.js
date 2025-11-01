3bf390507a30c7316dc4b845ec33a12c648a0126
Tanner Parkes <tanner.lp9@gmail.com>
2025-11-01 13:20:52 -0700

    Created tenative user and admin dashboards, the intent is currently that the admin dashboard loads in all the user information from the database and displays it in a table, so they can see name, email, and tokens. For the userDashboard, there is one GET request that will load in some of their information into a card so they can see their remaining tokens.

---
+++ Client/js/userdashboard.js
@@ -0,0 +1,29 @@
+class UserDashboard {
+
+    static async getUserInfo() 
+    {
+        try 
+        {
+            const response = await fetch('/api/user/info', 
+                {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (!response.ok) 
+            {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            document.getElementById('userTokenInfoDisplay').textContent = `You have ${data.tokens} tokens remaining.`;
+
+        }
+        catch (error) 
+        {
+            console.error('Error fetching user info:', error);
+        }
+
+    }
+}
